9570f0007eba75ce923bac0db08d2ce096778f9f
Hiroaki KARASAWA <siqvare@gmail.com>
2017-06-17 22:48:26 +0900

    Add ImagePicker

--- Gosenoku2/src/components/Edit.js
+++ Gosenoku2/src/components/Edit.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 import styled from 'styled-components/native';
 import { Container, Header, Left, Body, Title, Right, Content, Footer, FooterTab, Button, Icon, Input, Item } from 'native-base';
-
-const CenteredView = styled.View`
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-  flex-direction: column;
-`;
+// import ImagePicker from 'react-native-image-picker';
+var ImagePicker = require('react-native-image-picker');
 
 const styles = StyleSheet.create({
   container: {
@@ -22,9 +17,41 @@ const styles = StyleSheet.create({
 export default class Edit extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { imageSource: null };
   }
 
-  
+  addImageButtonPressed() {
+    const options = {
+      title: '画像を選択する',
+      customButtons: [ ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if(response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if(response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if(response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        let source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          imageSource: source
+        });
+      }
+    });
+  }
 
   render() {
     return (
@@ -36,14 +63,19 @@ export default class Edit extends React.Component {
           </Body>
           <Right />
         </Header>
+
         <Content contantContainerStyle={{ flexDirection: 'column', alignItems: 'center' }}>
           <Item underline>
             <Input placeholder='いまどうしてる？' style={{ height: 160, textAlignVertical: 'top', marginTop: 10 }} multiline={true}/>
           </Item>
-          <Button block light style={{ height: 100, marginTop: 10 }} onPress={ }>
-            <Text>2. 画像を追加する</Text>
+
+          { this.state.imageSource && <Image source={ this.state.imageSource} style={{ }} /> }
+
+          <Button block light style={{ height: 100, marginTop: 10 }} onPress={ this.addImageButtonPressed.bind(this) }>
+            <Text>{ this.state.imageSource ? 'もう一度とる' : '写真をとる' }</Text>
           </Button>
         </Content>
+
         <Footer>
           <FooterTab>
             <Button full>
