97d88af5427cfcb10a403594eefef5dd1936d5f6
Hiroaki KARASAWA <siqvare@gmail.com>
2017-06-18 16:38:39 +0900

    Remove Gosenoku version 1

--- Gosenoku/__tests__/step-sum.js
+++
@@ -1,5 +0,0 @@
- import StepSum from '../src/utils/step-sum';
-
-test('Add 1 to 5 to equal 15', () => {
-  expect(StepSum(5)).toBe(15);
-});
--- Gosenoku/src/utils/step-sum.js
+++
@@ -1,3 +0,0 @@
-export default (n) => {
-  return (1 + n) * n / 2;
-}
